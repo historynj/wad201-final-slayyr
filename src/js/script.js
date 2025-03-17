fac3d1032544278426858a39cb1d0e6d8c616284
SamirRamosSati <samirrssati@gmail.com>
2025-03-17 12:04:20 -0700

    Class | Add style (#50)
    * styled the title and the text behind it
    
    * styled the teachers section
    
    * styled the button and the list
    
    * fixed
    
    * updated the script file
    
    * updated the html file
    
    * final updates
    
    * removed the css files
    
    * added padding to the list

--- src/js/script.js
+++ src/js/script.js
@@ -1,41 +1,53 @@
 document.addEventListener("DOMContentLoaded", (event) => {
-    // get current path for nav current styling
-    const currentRaw = window.location.pathname;
-    const currentList = currentRaw.split('src');
-    const current = currentList.length > 1 ? currentList[1] : currentRaw;
+  // get current path for nav current styling
+  const currentRaw = window.location.pathname;
+  const currentList = currentRaw.split("src");
+  const current = currentList.length > 1 ? currentList[1] : currentRaw;
 
-    // render nav;
-    const body = document.getElementById('body');
+  // render nav;
+  const body = document.getElementById("body");
 
-    const nav = `
+  const nav = `
     <div class="nav__placeholder"></div>
     <nav id="nav" class="nav">
         <button class="nav__icon--hamburger">
             <img src="./images/icons/hamburger.png" alt="Menu drawer button" />
         </button>
-        <a href="/" class="nav__menu ${current == '/' ? 'current' : ''}">
+        <a href="/" class="nav__menu ${current == "/" ? "current" : ""}">
             <span>Home</span>
         </a>
-        <a href="./portfolio.html" class="nav__menu ${current.includes('/portfolio') ? 'current' : ''}">
+        <a href="./portfolio.html" class="nav__menu ${
+          current.includes("/portfolio") ? "current" : ""
+        }">
             <span>My Work</span>
         </a>
-        <a href="./service.html" class="nav__menu" ${current.includes('/service') ? 'current' : ''}>
+        <a href="./service.html" class="nav__menu" ${
+          current.includes("/service") ? "current" : ""
+        }>
             <span>Services</span>
         </a>
-        <a href="./academy.html" class="nav__menu" ${current.includes('/academy') ? 'current' : ''}>
+         <a href="./classes.html" class="nav__menu" ${
+           current.includes("/classes") ? "current" : ""
+         }>
             <span>Classes</span>
         </a>
         <a href="/" class="nav__logo">
             <img id="nav-logo-with-text" src="./images/logo/logo-with-text.png" alt="Carla Beauty Logo" />
             <img id="nav-logo" class="hidden" src="./images/logo/logo.png" alt="Carla Beauty Logo" />
         </a>
-        <a href="./beauty_tips.html" class="nav__menu ${current.includes('/beauty_tips') ? 'current' : ''}">
+        <a href="./beauty_tips.html" class="nav__menu ${
+          current.includes("/beauty_tips") ? "current" : ""
+        }">
             <span>Beauty Tips</span>
         </a>
-        <a href="./about.html" class="nav__menu ${current.includes('/about') ? 'current' : ''}">
+        <a href="./about.html" class="nav__menu ${
+          current.includes("/about") ? "current" : ""
+        }">
             <span>About</span>
         </a>
-        <a href="./contact.html" class="nav__menu ${current.includes('/contact') ? 'current' : ''}">
+        <a href="./contact.html" class="nav__menu ${
+          current.includes("/contact") ? "current" : ""
+        }">
             <span>Contact</span>
         </a>
         <a href="https://www.instagram.com/carlabeauty/?hl=en" target="_blank" class="nav__icon">
@@ -44,9 +56,9 @@ document.addEventListener("DOMContentLoaded", (event) => {
             </div>
         </a>
     </nav>
-    `
+    `;
 
-    const message = `
+  const message = `
         <ul id="message" class="message">
         <li class="message__item message__item--instagram invisiable">
             <a href="https://www.instagram.com/" target="_blank">
@@ -74,9 +86,9 @@ document.addEventListener("DOMContentLoaded", (event) => {
             </button>
         </li>
     </ul>
-    `
+    `;
 
-    const footer = `<footer id="footer" class="footer">
+  const footer = `<footer id="footer" class="footer">
         <div class="footer__top">
             <div class="footer__top__logo">
                 <img src="./images/logo/logo-with-text.png" alt="Carla Beauty logo" />
@@ -101,65 +113,65 @@ document.addEventListener("DOMContentLoaded", (event) => {
             </ul>
         </div>
         <ul class="footer__nav">
-            <li class="${current === '/' ? 'current' : ''}">  
+            <li class="${current === "/" ? "current" : ""}">  
                 <a href="#">Home</a>
             </li>
-            <li class="${current.includes('/portfolio') ? 'current' : ''}">
+            <li class="${current.includes("/portfolio") ? "current" : ""}">
                 <a href="./portfolio.html">My Work</a>
             </li>
-            <li class="${current.includes('/service') ? 'current' : ''}">
+            <li class="${current.includes("/service") ? "current" : ""}">
                 <a href="./service.html">Services</a>
             </li>
-            <li class="${current.includes('/academy') ? 'current' : ''}">
+            <li class="${current.includes("/academy") ? "current" : ""}">
                 <a href="./academy.html">Classes</a>
             </li>
-            <li class="${current.includes('/beauty_tips') ? 'current' : ''}">
+            <li class="${current.includes("/beauty_tips") ? "current" : ""}">
                 <a href="./beauty_tips.html">Beauty Tips</a>
             </li>
-            <li class="${current.includes('/about') ? 'current' : ''}">
+            <li class="${current.includes("/about") ? "current" : ""}">
                 <a href="./about.html">About</a>
             </li>
-            <li class="${current.includes('/contact') ? 'current' : ''}">
+            <li class="${current.includes("/contact") ? "current" : ""}">
                 <a href="./contact.html">Contact</a>
             </li>
         </ul>
         <p class="footer__copyright">Copyright 2024, Carla Beauty</p>
-    </footer>`
+    </footer>`;
 
-    body.innerHTML = nav + message + body.innerHTML + footer;
+  body.innerHTML = nav + message + body.innerHTML + footer;
 });
 
-window.addEventListener('scroll', () => {
-    const nav = document.getElementById('nav');
-    const scrollTop = window.scrollY;
+window.addEventListener("scroll", () => {
+  const nav = document.getElementById("nav");
+  const scrollTop = window.scrollY;
 
-    requestAnimationFrame(() => {
-        if (scrollTop > 50) {
-            nav?.classList.add('scrolled');
-        } else {
-            nav?.classList.remove('scrolled');
-        }
-    });
+  requestAnimationFrame(() => {
+    if (scrollTop > 50) {
+      nav?.classList.add("scrolled");
+    } else {
+      nav?.classList.remove("scrolled");
+    }
+  });
 });
 
 function openMessage() {
-    const menus = document.querySelectorAll('.message__item');
-    menus.forEach((menu) => {
-        if (menu?.classList.contains('message__item--message')) {
-            menu?.classList.add('hidden');
-        } else {
-            menu?.classList.remove('invisiable');
-        }
-    })
+  const menus = document.querySelectorAll(".message__item");
+  menus.forEach((menu) => {
+    if (menu?.classList.contains("message__item--message")) {
+      menu?.classList.add("hidden");
+    } else {
+      menu?.classList.remove("invisiable");
+    }
+  });
 }
 
 function closeMessage() {
-    const menus = document.querySelectorAll('.message__item');
-    menus.forEach((menu) => {
-        if (menu?.classList.contains('message__item--message')) {
-            menu?.classList.remove('hidden');
-        } else {
-            menu?.classList.add('invisiable');
-        }
-    })
+  const menus = document.querySelectorAll(".message__item");
+  menus.forEach((menu) => {
+    if (menu?.classList.contains("message__item--message")) {
+      menu?.classList.remove("hidden");
+    } else {
+      menu?.classList.add("invisiable");
+    }
+  });
 }
