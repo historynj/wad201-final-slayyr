19e794d0ce6597d7113f4b60b53a8f2525ba66a7
Luiz Eduardo Marques Pereira <luize.mape@gmail.com>
2025-03-19 21:55:49 -0700

    Nav | Add mobile nav (#61)
    * created JS and creating nav/hamb-btn on html
    
    * needs to fix whats wrong in my code
    
    * trying to fix hamburguer btn
    
    * created nav on index.html
    
    * just need to fix padding and heights
    
    * still need to adjust the last details
    
    * Fix mobile nav open/close & styling
    
    ---------
    
    Co-authored-by: lynj <navyoonj@gmail.com>

--- src/js/script.js
+++ src/js/script.js
@@ -1,11 +1,11 @@
-document.addEventListener("DOMContentLoaded", (event) => {
+document.addEventListener('DOMContentLoaded', (event) => {
   // get current path for nav current styling
   const currentRaw = window.location.pathname;
-  const currentList = currentRaw.split("src");
+  const currentList = currentRaw.split('src');
   const current = currentList.length > 1 ? currentList[1] : currentRaw;
 
   // render nav;
-  const body = document.getElementById("body");
+  const body = document.getElementById('body');
 
   const nav = `
     <div class="nav__placeholder"></div>
@@ -13,16 +13,16 @@ document.addEventListener("DOMContentLoaded", (event) => {
         <button class="nav__icon--hamburger">
             <img src="./images/icons/hamburger.png" alt="Menu drawer button" />
         </button>
-        <a href="/" class="nav__menu ${current == "/" ? "current" : ""}">
+        <a href="/" class="nav__menu ${current == '/' ? 'current' : ''}">
             <span>Home</span>
         </a>
         <a href="./portfolio.html" class="nav__menu ${
-          current.includes("/portfolio") ? "current" : ""
+          current.includes('/portfolio') ? 'current' : ''
         }">
             <span>My Work</span>
         </a>
         <a href="./service.html" class="nav__menu" ${
-          current.includes("/service") ? "current" : ""
+          current.includes('/service') ? 'current' : ''
         }>
             <span>Services</span>
         </a>
@@ -32,21 +32,20 @@ document.addEventListener("DOMContentLoaded", (event) => {
             <span>Classes</span>
         </a>
         <a href="/" class="nav__logo">
-            <img id="nav-logo-with-text" src="./images/logo/logo-with-text.png" alt="Carla Beauty Logo" />
-            <img id="nav-logo" class="hidden" src="./images/logo/logo.png" alt="Carla Beauty Logo" />
+            <img src="./images/logo/logo-with-text.png" alt="Carla Beauty Logo" />
         </a>
         <a href="./beauty_tips.html" class="nav__menu ${
-          current.includes("/beauty_tips") ? "current" : ""
+          current.includes('/beauty_tips') ? 'current' : ''
         }">
             <span>Beauty Tips</span>
         </a>
         <a href="./about.html" class="nav__menu ${
-          current.includes("/about") ? "current" : ""
+          current.includes('/about') ? 'current' : ''
         }">
             <span>About</span>
         </a>
         <a href="./contact.html" class="nav__menu ${
-          current.includes("/contact") ? "current" : ""
+          current.includes('/contact') ? 'current' : ''
         }">
             <span>Contact</span>
         </a>
@@ -56,7 +55,73 @@ document.addEventListener("DOMContentLoaded", (event) => {
             </div>
         </a>
     </nav>
-    `;
+    <nav id="mobile-nav" class="mobile-nav close">
+      <div class="mobile-nav__header">
+        <button id="close-button" class="icon--close">
+          <img
+            src="./images/icons/close.png"
+            alt=""
+          />
+        </button>
+        <div class="mobile-nav__logo">
+          <img
+            src="./images/logo/logo-with-text.png"
+            alt=""
+          />
+        </div>
+      </div>
+      <div class="h-full">
+        <ul class="mobile-nav__list h-full">
+          <li class="mobile-nav__item">
+            <a id="home-page" href="./">Home</a>
+          </li>
+          <li class="mobile-nav__item">
+            <a id="mywork-page" href="./portfolio.html">My Work</a>
+          </li>
+          <li class="mobile-nav__item">
+            <a id="services-page" href="./service.html">Services</a>
+          </li>
+          <li class="mobile-nav__item">
+            <a id="classses-page" href="./academy.html">Classes</a>
+          </li>
+          <li class="mobile-nav__item">
+            <a id="beautytips-page" href="./beauty_tips.html">Beauty Tips</a>
+          </li>
+          <li class="mobile-nav__item">
+            <a id="about-page" href="./about.html">About</a>
+          </li>
+          <li class="mobile-nav__item">
+            <a id="contact-page" href="./contact.html">Contact</a>
+          </li>
+        </ul>
+      </div>
+      <div class="mobile-nav__footer">
+        <div class="mobile-nav__footer__social-media">
+          <div>
+            <img
+              src="./images/service/ph_phone.png"
+              alt=""
+            />
+          </div>
+          <div>
+            <img
+              src="./images/service/Whatsapp.png"
+              alt=""
+            />
+          </div>
+          <div>
+            <img
+              src="./images/service/instagram.png"
+              alt=""
+            />
+          </div>
+        </div>
+        <div class="mobile-nav__footer__caption">
+          <p class="expert-p">Expert Makeup and Hairstyle Studio</p>
+        </div>
+      </div>
+    </nav>
+  `;
 
   const message = `
         <ul id="message" class="message">
@@ -113,25 +178,25 @@ document.addEventListener("DOMContentLoaded", (event) => {
             </ul>
         </div>
         <ul class="footer__nav">
-            <li class="${current === "/" ? "current" : ""}">  
+            <li class="${current === '/' ? 'current' : ''}">  
                 <a href="#">Home</a>
             </li>
-            <li class="${current.includes("/portfolio") ? "current" : ""}">
+            <li class="${current.includes('/portfolio') ? 'current' : ''}">
                 <a href="./portfolio.html">My Work</a>
             </li>
-            <li class="${current.includes("/service") ? "current" : ""}">
+            <li class="${current.includes('/service') ? 'current' : ''}">
                 <a href="./service.html">Services</a>
             </li>
-            <li class="${current.includes("/academy") ? "current" : ""}">
+            <li class="${current.includes('/academy') ? 'current' : ''}">
                 <a href="./academy.html">Classes</a>
             </li>
-            <li class="${current.includes("/beauty_tips") ? "current" : ""}">
+            <li class="${current.includes('/beauty_tips') ? 'current' : ''}">
                 <a href="./beauty_tips.html">Beauty Tips</a>
             </li>
-            <li class="${current.includes("/about") ? "current" : ""}">
+            <li class="${current.includes('/about') ? 'current' : ''}">
                 <a href="./about.html">About</a>
             </li>
-            <li class="${current.includes("/contact") ? "current" : ""}">
+            <li class="${current.includes('/contact') ? 'current' : ''}">
                 <a href="./contact.html">Contact</a>
             </li>
         </ul>
@@ -139,39 +204,64 @@ document.addEventListener("DOMContentLoaded", (event) => {
     </footer>`;
 
   body.innerHTML = nav + message + body.innerHTML + footer;
+
+  // mobile nav manipulation
+  const hamburguerButton = document.querySelector('.nav__icon--hamburger');
+  const mobileNav = document.getElementById('mobile-nav');
+  const closeButton = document.getElementById('close-button');
+  const navList = document.querySelectorAll('.nav-list li');
+
+  // adding event (click hamburguer btn)
+  hamburguerButton.addEventListener('click', function () {
+    mobileNav.classList.remove('close');
+    body.classList.add('no-scroll');
+  });
+  // adding event (click close-btn)
+  closeButton.addEventListener('click', function () {
+    mobileNav.classList.add('close');
+    console.log('close button clicked');
+    body.classList.remove('no-scroll');
+  });
+
+  navList.forEach((page) => {
+    page.addEventListener('click', function () {
+      console.log('Clicked');
+      mobileNav.classList.add('close');
+    });
+  });
+
+  window.addEventListener('scroll', () => {
+    const nav = document.getElementById('nav');
+    const scrollTop = window.scrollY;
+
+    requestAnimationFrame(() => {
+      if (scrollTop > 50) {
+        nav?.classList.add('scrolled');
+      } else {
+        nav?.classList.remove('scrolled');
+      }
+    });
+  });
+
+  function openMessage() {
+    const menus = document.querySelectorAll('.message__item');
+    menus.forEach((menu) => {
+      if (menu?.classList.contains('message__item--message')) {
+        menu?.classList.add('close');
+      } else {
+        menu?.classList.remove('invisiable');
+      }
+    });
+  }
+
+  function closeMessage() {
+    const menus = document.querySelectorAll('.message__item');
+    menus.forEach((menu) => {
+      if (menu?.classList.contains('message__item--message')) {
+        menu?.classList.remove('close');
+      } else {
+        menu?.classList.add('invisiable');
+      }
+    });
+  }
 });
-
-window.addEventListener("scroll", () => {
-  const nav = document.getElementById("nav");
-  const scrollTop = window.scrollY;
-
-  requestAnimationFrame(() => {
-    if (scrollTop > 50) {
-      nav?.classList.add("scrolled");
-    } else {
-      nav?.classList.remove("scrolled");
-    }
-  });
-});
-
-function openMessage() {
-  const menus = document.querySelectorAll(".message__item");
-  menus.forEach((menu) => {
-    if (menu?.classList.contains("message__item--message")) {
-      menu?.classList.add("hidden");
-    } else {
-      menu?.classList.remove("invisiable");
-    }
-  });
-}
-
-function closeMessage() {
-  const menus = document.querySelectorAll(".message__item");
-  menus.forEach((menu) => {
-    if (menu?.classList.contains("message__item--message")) {
-      menu?.classList.remove("hidden");
-    } else {
-      menu?.classList.add("invisiable");
-    }
-  });
-}
